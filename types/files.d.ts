0ace3e5da39b595e2a21d4c7cc420f6d0a0d4812
Chin <greeting.to.chin@gmail.com>
2021-03-11 10:44:42 +0800

    Add types

---
+++ types/files.d.ts
@@ -0,0 +1,36 @@
+declare module '*.png' {
+  const _: string;
+  export default _;
+}
+declare module '*.jpg' {
+  const _: string;
+  export default _;
+}
+declare module '*.jpeg' {
+  const _: string;
+  export default _;
+}
+declare module '*.gif' {
+  const _: string;
+  export default _;
+}
+
+declare module '*.eot' {
+  const _: string;
+  export default _;
+}
+
+declare module '*.ttf' {
+  const _: string;
+  export default _;
+}
+
+declare module '*.woff' {
+  const _: string;
+  export default _;
+}
+
+declare module '*.woff2' {
+  const _: string;
+  export default _;
+}
