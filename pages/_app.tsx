a5490bd0f405b1b7575f62a54d0ce46ffd5db9a9
Chin <greeting.to.chin@gmail.com>
2021-04-27 08:45:50 +0800

    chore: store QueryClient in ref

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import type { AppProps } from 'next/app';
+import { useRef } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import 'styles/globals.scss';
 
-const queryClient = new QueryClient();
-
 function App({ Component, pageProps }: AppProps): JSX.Element {
+  const queryClientRef: { current?: QueryClient } = useRef();
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClientRef.current}>
       <Component {...pageProps} />
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen />
+      )}
     </QueryClientProvider>
   );
 }
