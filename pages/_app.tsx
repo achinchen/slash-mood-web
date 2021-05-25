d5a36a89969c949efd86c6e40db80a029d620983
Chin <greeting.to.chin@gmail.com>
2021-05-25 19:20:57 +0800

    chore(app): remove unused query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,14 @@
 import type { AppProps } from 'next/app';
-import { useRef } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import 'styles/globals.scss';
 
 export const PORTAL_ID = 'portal';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  const queryClientRef: { current?: QueryClient } = useRef();
-
-  if (!queryClientRef.current) {
-    queryClientRef.current = new QueryClient();
-  }
-
   return (
-    <QueryClientProvider client={queryClientRef.current}>
+    <>
       <Component {...pageProps} />
       <div id={PORTAL_ID} />
-    </QueryClientProvider>
+    </>
   );
 }
 
